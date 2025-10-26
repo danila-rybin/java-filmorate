f7abdcc3330c7c5952811377697989b15295839b
Danila Rybin <rybin.danila@gmail.com>
2025-10-26 19:05:59 +0300

    Добавление пробелов в FilmService для прохождения проверок гитхаба на кодстайл 2

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -4,13 +4,17 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserService {
+
     User createUser(User user);
+
     User updateUser(User user);
 
     void addFriend(long userId, long friendId);
+
     void removeFriend(long userId, long friendId);
 
     List<User> getFriends(long userId);
+
     List<User> getCommonFriends(long userId, long otherId);
 
     User getUserById(long id);
