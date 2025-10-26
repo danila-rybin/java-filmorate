36c92da42e20ac2861d44ea0f60e15baacd5afcf
Danila Rybin <rybin.danila@gmail.com>
2025-10-26 19:10:07 +0300

    Добавление пробелов в FilmService для прохождения проверок гитхаба на кодстайл 4

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -8,9 +8,9 @@ import java.util.Optional;
 
 public interface FilmStorage {
 
-    Film create( Film film) ;
+    Film create(Film film);
 
-    Film update( Film film);
+    Film update(Film film);
 
     List<Film> findAll();
 
