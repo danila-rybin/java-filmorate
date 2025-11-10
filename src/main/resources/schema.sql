2ec137b53e41e9129a19a6b4c0714de15c31fc22
Рыбин Данила <dgrybin@localadmins-MacBook-Pro.local>
2025-11-10 17:34:13 +0300

    добавил диаграмму DB в файл README.md

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,15 @@
--- Таблица рейтингов MPA
+
 CREATE TABLE IF NOT EXISTS mpa_ratings (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(10) NOT NULL UNIQUE,
     description VARCHAR(255)
     );
 
--- Таблица жанров
 CREATE TABLE IF NOT EXISTS genres (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL UNIQUE
     );
 
--- Основная таблица фильмов
 CREATE TABLE IF NOT EXISTS films (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -22,7 +20,6 @@ CREATE TABLE IF NOT EXISTS films (
     FOREIGN KEY (mpa_id) REFERENCES mpa_ratings(id)
     );
 
--- Связующая таблица фильмов и жанров
 CREATE TABLE IF NOT EXISTS film_genres (
     film_id INTEGER NOT NULL,
     genre_id INTEGER NOT NULL,
@@ -31,7 +28,6 @@ CREATE TABLE IF NOT EXISTS film_genres (
     FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
     );
 
--- Таблица пользователей
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -40,7 +36,6 @@ CREATE TABLE IF NOT EXISTS users (
     birthday DATE
     );
 
--- Таблица дружбы с статусами
 CREATE TABLE IF NOT EXISTS friendships (
     user_id INTEGER NOT NULL,
     friend_id INTEGER NOT NULL,
@@ -52,7 +47,6 @@ CREATE TABLE IF NOT EXISTS friendships (
     CHECK (user_id != friend_id)
     );
 
--- Таблица лайков
 CREATE TABLE IF NOT EXISTS likes (
     film_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
